b72d268e53dbe46755c8c150c2bd72c6fdcf26bb
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2018-04-05 15:05:57 +0200

    One dice changes number randomly

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -29,7 +31,15 @@ class ViewController: UIViewController {
         
         // Function generates random numbers between 0 - 5
         randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+         
     }
+    
+    
     
 }
 
