a9f495e30fffeac75cf3085a0bd2256cdcf6de44
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2018-04-05 15:14:26 +0200

    Code refactoring: More readable

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -18,8 +18,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
+        // Only triggered when view loads up
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +31,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         // Function generates random numbers between 0 - 5
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
@@ -37,9 +43,8 @@ class ViewController: UIViewController {
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
-    
-    
     
 }
 
